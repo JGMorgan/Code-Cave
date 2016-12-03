e986e7922dff5a59550c7d4a98bf0453c4354a9d
Jose Morgan <jose@Joses-MacBook-Pro.local>
2016-12-03 14:58:32 -0500

    front end looks decent and backend displays errors and stuff

--- backend/router.go
+++ backend/router.go
@@ -1,16 +1,64 @@
 package main
 
 import (
-	"io"
+	//"io"
 	"net/http"
     "log"
     "os/exec"
+    "encoding/json"
     "bytes"
+    //"fmt"
+    //"io/ioutil"
+    "os"
 )
 
+type Code struct {
+    Language string
+    Content string
+}
+
+type STDOut struct {
+    Language string
+    Output string
+    Error string
+}
+
+func handleCors(w http.ResponseWriter) {
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+    w.Header().Set("Access-Control-Max-Age", "3600")
+    w.Header().Set("Access-Control-Allow-Headers",
+        "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+}
+
 func runGo(w http.ResponseWriter, r *http.Request) {
+    handleCors(w)
+
+    var code Code
+
+    buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+    json.Unmarshal(buf.Bytes(), &code)
+
+    f, err := os.Create("./test.go")
+    if err != nil {
+		log.Fatal(err)
+	}
+
+    _, err = f.WriteString(code.Content)
+    if err != nil {
+		log.Fatal(err)
+	}
+
+    f.Sync()
+    f.Close()
+
     cmd := exec.Command("go", "run", "test.go")
 	stdout, err := cmd.StdoutPipe()
+    if err != nil {
+		log.Fatal(err)
+	}
+    stderr, err := cmd.StderrPipe()
     if err != nil {
 		log.Fatal(err)
 	}
@@ -19,10 +67,15 @@ func runGo(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-    buf := new(bytes.Buffer)
-	buf.ReadFrom(stdout)
-
-    io.WriteString(w, buf.String())
+    stdoutbuf := new(bytes.Buffer)
+	stdoutbuf.ReadFrom(stdout)
+    stderrbuf := new(bytes.Buffer)
+	stderrbuf.ReadFrom(stderr)
+    out := STDOut{Language: "Go",
+        Output: stdoutbuf.String(),
+        Error: stderrbuf.String()}
+    b, err := json.Marshal(out)
+    w.Write(b)
 }
 
 func main() {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,80 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var CodeMirror = require('react-codemirror');
 
+require('codemirror/lib/codemirror.css');
+require('codemirror/mode/go/go');
+require('codemirror/theme/solarized.css')
+require('./index.css')
+
 var App = React.createClass({
     getInitialState: function() {
         return {
             code: "// Code",
+            stdout: "",
+            stderr: ""
         };
     },
     updateCode: function(newCode) {
         this.setState({
             code: newCode,
+            stdout: this.state.stdout,
+            stderr: this.state.stderr
+        });
+    },
+    updateStdOut: function(newOut) {
+        this.setState({
+            code: this.state.code,
+            stdout: newOut,
+            stderr: this.state.stderr
+        });
+    },
+    updateStdErr: function(newErr) {
+        this.setState({
+            code: this.state.code,
+            stdout: this.state.stdout,
+            stderr: newErr
+        });
+    },
+    runCode: function() {
+        var self = this;
+        fetch('http://localhost:8000/run/GoLang', {
+            method: 'POST',
+            headers: {
+                'Accept': '*/*',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                Language: "Go",
+                Content: this.state.code
+            })
+        }).then((response) => {
+            return response.json();
+        }).then(function(data) {
+            console.log(data);
+            self.updateStdOut(data.Output)
+            self.updateStdErr(data.Error)
         });
     },
     render: function() {
         var options = {
             lineNumbers: true,
+            theme: 'solarized dark',
+            mode: 'go'
         };
-        return <CodeMirror value={this.state.code} onChange={this.updateCode} options={options} />
+        return (
+            <div className="flex-container">
+                <div className="flex-item">
+                    <CodeMirror value={this.state.code} onChange={this.updateCode} options={options} />
+                </div>
+                <div className="tiny-flex"></div>
+                <div className="flex-item" style={{backgroundColor: "#002b36"}}>
+                    <button onClick={this.runCode}> Run </button>
+                    <pre style={{color: "#FFFFFF", fontFamily: 'Courier New'}}>{this.state.stdout}</pre>
+                    <pre style={{color: "#FFFFFF", fontFamily: 'Courier New'}}>{this.state.stderr}</pre>
+                </div>
+            </div>
+        );
     }
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('app'));
