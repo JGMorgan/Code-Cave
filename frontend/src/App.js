b417a1ac19ca88a93a69c0579693b88879a1f0f2
Jose Morgan <jose@Joses-MacBook-Pro.local>
2016-12-11 22:53:18 -0500

    resizing kinda works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var CodeMirror = require('react-codemirror');
+var Draggable = require('react-draggable');
 
 require('codemirror/lib/codemirror.css');
 require('codemirror/mode/go/go');
@@ -12,28 +13,36 @@ var App = React.createClass({
         return {
             code: "// Code",
             stdout: "",
-            stderr: ""
+            stderr: "",
+            leftFlex: .5,
+            rightFlex: .5
         };
     },
     updateCode: function(newCode) {
         this.setState({
             code: newCode,
             stdout: this.state.stdout,
-            stderr: this.state.stderr
+            stderr: this.state.stderr,
+            leftFlex: this.state.leftFlex,
+            rightFlex: this.state.rightFlex
         });
     },
     updateStdOut: function(newOut) {
         this.setState({
             code: this.state.code,
             stdout: newOut,
-            stderr: this.state.stderr
+            stderr: this.state.stderr,
+            leftFlex: this.state.leftFlex,
+            rightFlex: this.state.rightFlex
         });
     },
     updateStdErr: function(newErr) {
         this.setState({
             code: this.state.code,
             stdout: this.state.stdout,
-            stderr: newErr
+            stderr: newErr,
+            leftFlex: this.state.leftFlex,
+            rightFlex: this.state.rightFlex
         });
     },
     runCode: function() {
@@ -56,6 +65,27 @@ var App = React.createClass({
             self.updateStdErr(data.Error)
         });
     },
+    clear: function() {
+        this.setState({
+            code: this.state.code,
+            stdout: "",
+            stderr: "",
+            leftFlex: this.state.leftFlex,
+            rightFlex: this.state.rightFlex
+        });
+    },
+    handleDrag: function(e, position) {
+        this.setState({
+            code: this.state.code,
+            stdout: "",
+            stderr: "",
+            leftFlex: (position.x + window.innerWidth/2) / window.innerWidth,
+            rightFlex: 1 - (position.x + window.innerWidth/2) / window.innerWidth
+        });
+        console.log(position.x);
+        console.log(window.innerWidth);
+        console.log((position.x + window.innerWidth/2) / window.innerWidth);
+    },
     render: function() {
         var options = {
             lineNumbers: true,
@@ -63,15 +93,35 @@ var App = React.createClass({
             mode: 'go'
         };
         return (
-            <div className="flex-container">
-                <div className="flex-item">
-                    <CodeMirror value={this.state.code} onChange={this.updateCode} options={options} />
+            <div ref="parentDiv">
+                <div>
+                    <button onClick={this.runCode} className="run-button"> Run &#9658; </button>
+                    <button onClick={this.clear} className="run-button"> Clear </button>
+                    <select className="lang-select">
+                        <option value="Go">Go</option>
+                    </select>
                 </div>
-                <div className="tiny-flex"></div>
-                <div className="flex-item" style={{backgroundColor: "#002b36"}}>
-                    <button onClick={this.runCode}> Run </button>
-                    <pre style={{color: "#FFFFFF", fontFamily: 'Courier New'}}>{this.state.stdout}</pre>
-                    <pre style={{color: "#FFFFFF", fontFamily: 'Courier New'}}>{this.state.stderr}</pre>
+                <div className="flex-container">
+                    <div className="flex-item" style={{flex: this.state.leftFlex.toString()}}>
+                        <CodeMirror value={this.state.code} onChange={this.updateCode} options={options} />
+                    </div>
+                    <Draggable
+                     axis="x"
+                     handle=".handle"
+                     bounds="body"
+                     defaultPosition={{x: 0, y: 0}}
+                     position={null}
+                     onStart={this.handleStart}
+                     onDrag={this.handleDrag}
+                     onStop={this.handleStop}>
+                        <div className="handle">
+                            <div className="tiny-flex" style={{cursor: 'col-resize'}}></div>
+                        </div>
+                    </Draggable>
+                    <div className="flex-item" style={{backgroundColor: "#002b36", flex: this.state.rightFlex.toString()}}>
+                        <pre style={{color: "#FFFFFF", fontFamily: 'Courier New'}}>{this.state.stdout}</pre>
+                        <pre style={{color: "#FFFFFF", fontFamily: 'Courier New'}}>{this.state.stderr}</pre>
+                    </div>
                 </div>
             </div>
         );
